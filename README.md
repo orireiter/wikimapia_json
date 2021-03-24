6e76a046b7e752b2c74dbe5270a66bd8423817ec
orireiter <60241083+orireiter@users.noreply.github.com>
2021-03-25 00:36:29 +0200

    added readme, added commandline args, check readme

---
+++ README.md
@@ -0,0 +1,35 @@
+# Wikimapia scraper
+A service that scrapes points of interest in a given country.
+
+---
+## Installation
+First, install docker and dokcer-compose. \
+After cloning the repository, from its main folder, cd into docker and again into docker-compose. There, execute docker-compose up:
+```bash
+$ cd ./docker/docker-compose
+$ docker-compose up
+```
+Then, in a new terminal, cd back to the repository's main folder, and run the main script along with a country and an output json:
+```bash
+$ python ./source_code/main.py france france.json
+```
+---
+## Architecture
+This tool consists of 3 main pieces:
+* MongoDB container
+* Tor container
+* Main Script
+
+To use it, first run docker-compose which will run the two containers.\
+Then, when running the script from the repository's main folder (as shown above), the script will check the DB to see if the data of the given country has already been collected.\
+If it hasn't, wikimapia will be scraped for the said data, and will insert it to the DB.\
+The need for the tor container is to bypass the connection limit to the server (which is set by IP). Tor acts as a proxy.\
+In either case, a GeoJSON will be created in the file specified in the CLI.
+
+---
+## Configuration
+The configuration files in this project are the __docker-compose.yml__ (which will usually work fine without modification), and the __.env__ (which also should work).
+
+
+
+
--- source_code/main.py
+++ source_code/main.py
@@ -1,5 +1,6 @@
 from json import dumps
 import os
+import sys
 import datetime
 
 from dotenv import load_dotenv
@@ -12,58 +13,67 @@ from utilities.proxy_connection import TorRequests
 
 
 if __name__ == '__main__':
-    # First, the .env file is loaded into the process.
-    load_dotenv()
+    if len(sys.argv) <= 2:
+        print('Please supply a country and an output file in the CLI when '
+              'running the script.')
 
-    # Assigning env vars into constants.
-    WIKIMAPIA_COUNTRIES_PAGE = os.environ.get('WIKIMAPIA_COUNTRIES_PAGE')
-    COUNTRY = os.environ.get('COUNTRY')
-    LAYERS = int(os.environ.get('LAYERS'))
-
-    CONNECTION_STRING = os.environ.get('CONNECTION_STRING')
-    DATABASE = os.environ.get('DATABASE')
-
-    TOR_HOST = os.environ.get('TOR_HOST')
-    TOR_MANAGEMENT_PORT = int(os.environ.get('TOR_MANAGEMENT_PORT'))
-    TOR_MANAGEMENT_PASSWORD = os.environ.get('TOR_MANAGEMENT_PASSWORD')
-    TOR_PORT = int(os.environ.get('TOR_PORT'))
-    TOR_SWITCH_IP_EVERY = int(os.environ.get('TOR_SWITCH_IP_EVERY'))
-
-    OUTPUT_FILE = os.environ.get('OUTPUT_FILE')
-
-    # Start writing the output file in geojson format.
-    with open(OUTPUT_FILE, 'w') as file:
-        file.write('''{
-            "type": "FeatureCollection",
-            "features": [
-                ''')
-
-    # If a collection of the desired country already exists in mongo,
-    # it means it was collected before. Instead of rescraping, the db is used
-    #  to create the geojson.
-    if is_collection(CONNECTION_STRING, DATABASE, COUNTRY):
-        db_connection = db_connect(CONNECTION_STRING, DATABASE, COUNTRY)
-        results = db_connection.find({}, projection={'_id': False})
-        with open(OUTPUT_FILE, 'a') as file:
-            for result in results:
-                file.write(dumps(result)+',\n')
-
-    # Otherwise, the country's page is scraped. It is needed to get to a deeper
-    # layer of the website, so recursion is used.
-    # Once getting to a point of interest, it executes to
-    # callback function over it, alongside the given kwargs.
-    # The callback function will create a geojson from the point of interest
-    #  and append it into the output file, and isnert it to the db.
     else:
-        proxied_connection = TorRequests(
-            TOR_HOST, TOR_MANAGEMENT_PORT, TOR_MANAGEMENT_PASSWORD, TOR_PORT, TOR_SWITCH_IP_EVERY)
-        recursive_executer(get_wikimapia_links_from_html, iterate_function, LAYERS,
-                           WIKIMAPIA_COUNTRIES_PAGE+COUNTRY+'/', callback=GeoScraper,
-                           mongo_connection_string=CONNECTION_STRING, db=DATABASE,
-                           collection=COUNTRY, output_file=OUTPUT_FILE, requests_session=proxied_connection)
+        country = sys.argv[1]
+        output_file = sys.argv[2]
 
-    #  This is needed to close the geojson.
-    with open(OUTPUT_FILE, 'a') as file:
-        file.write(''']}''')
+        # First, the .env file is loaded into the process.
+        load_dotenv()
 
-    print(f'{datetime.datetime.now()} -> INFO: Done scraping {COUNTRY}!')
+        # Assigning env vars into constants.
+        WIKIMAPIA_COUNTRIES_PAGE = os.environ.get('WIKIMAPIA_COUNTRIES_PAGE')
+        LAYERS = int(os.environ.get('LAYERS'))
+
+        CONNECTION_STRING = os.environ.get('CONNECTION_STRING')
+        DATABASE = os.environ.get('DATABASE')
+
+        TOR_HOST = os.environ.get('TOR_HOST')
+        TOR_MANAGEMENT_PORT = int(os.environ.get('TOR_MANAGEMENT_PORT'))
+        TOR_MANAGEMENT_PASSWORD = os.environ.get('TOR_MANAGEMENT_PASSWORD')
+        TOR_PORT = int(os.environ.get('TOR_PORT'))
+        TOR_SWITCH_IP_EVERY = int(os.environ.get('TOR_SWITCH_IP_EVERY'))
+
+        # Start writing the output file in geojson format.
+        with open(output_file, 'w') as file:
+            file.write('''{
+                "type": "FeatureCollection",
+                "features": [
+                    ''')
+
+        # If a collection of the desired country already exists in mongo,
+        # it means it was collected before. Instead of rescraping, the db is used
+        #  to create the geojson.
+        if is_collection(CONNECTION_STRING, DATABASE, country):
+            print(f'{datetime.datetime.now()} -> INFO: {country} already in DB,'
+                  ' retrieving it.')
+            db_connection = db_connect(CONNECTION_STRING, DATABASE, country)
+            results = db_connection.find({}, projection={'_id': False})
+            with open(output_file, 'a') as file:
+                for result in results:
+                    file.write(dumps(result)+',\n')
+
+        # Otherwise, the country's page is scraped. It is needed to get to a deeper
+        # layer of the website, so recursion is used.
+        # Once getting to a point of interest, it executes to
+        # callback function over it, alongside the given kwargs.
+        # The callback function will create a geojson from the point of interest
+        #  and append it into the output file, and isnert it to the db.
+        else:
+            proxied_connection = TorRequests(
+                TOR_HOST, TOR_MANAGEMENT_PORT, TOR_MANAGEMENT_PASSWORD, TOR_PORT, TOR_SWITCH_IP_EVERY)
+            print(f'{datetime.datetime.now()} -> INFO: {country} not in db,'
+                  ' starting to scrape.')
+            recursive_executer(get_wikimapia_links_from_html, iterate_function, LAYERS,
+                               WIKIMAPIA_COUNTRIES_PAGE+country+'/', callback=GeoScraper,
+                               mongo_connection_string=CONNECTION_STRING, db=DATABASE,
+                               collection=country, output_file=output_file, requests_session=proxied_connection)
+
+        #  This is needed to close the geojson.
+        with open(output_file, 'a') as file:
+            file.write(''']}''')
+
+        print(f'{datetime.datetime.now()} -> INFO: Done scraping {country}!')
